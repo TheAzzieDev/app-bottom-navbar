ae9cab6b11a47f37d43647a826cfcace33585c8b
Jonathan Cano Sjöstedt <asrielrosemary@gmail.com>
2024-07-23 16:51:22 +0200

    Took way to long to just change some stupid colors

--- app/src/main/java/com/example/bottom_navigationbar_tutorial/DesginTest.java
+++ app/src/main/java/com/example/bottom_navigationbar_tutorial/DesginTest.java
@@ -1,14 +1,19 @@
 package com.example.bottom_navigationbar_tutorial;
 
+import static java.security.AccessController.getContext;
+
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,49 +34,34 @@ public class DesginTest extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding =  BottomNavbardesginterstBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        /*
 
-        nav_settings
-        nav_gallery
-         */
-        FloatingActionButton a = findViewById(R.id.floatingActionButton);
-
-        binding.bottomNavigationView.setOnItemSelectedListener(item ->{
-            switch (item.getItemId()){
-                case R.id.nav_settings:
-                    Log.d("testing", "hey");
-                    item.setIconTintList(ColorStateList.valueOf(Color.RED));
-                    break;
-            }
-            return true;
-        });
-        /*
-        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
-        fab.setOnClickListener(view -> {
-            Intent intent = new Intent(this, DesginTest.class);
-            startActivity(intent);
-        });
-        */
-
-
-        a.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.color_test));
-        a.setImageTintList(ContextCompat.getColorStateList(DesginTest.this, R.color.solidGreen));
-        a.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (a.getBackgroundTintList() == ContextCompat.getColorStateList(DesginTest.this, R.color.selectorGreen)){
-                    a.setBackgroundTintList(ContextCompat.getColorStateList(DesginTest.this, R.color.lightGreen));
-                    a.setImageTintList(ContextCompat.getColorStateList(DesginTest.this, R.color.solidGreen));
-                }
-                else{
-                    a.setBackgroundTintList(ContextCompat.getColorStateList(DesginTest.this, R.color.selectorGreen));
-                    a.setImageTintList(ContextCompat.getColorStateList(DesginTest.this, R.color.lightGreen));
-                }
-            }
-        });
+        binding.camera.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.lightGreen));
+        binding.camera.setImageTintList(ContextCompat.getColorStateList(DesginTest.this, R.color.solidGreen));
     }
 
 
+    public void navClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.camera:
+                binding.camera.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.selectorGreen));
+                binding.camera.setImageTintList(ContextCompat.getColorStateList(DesginTest.this, R.color.lightGreen));
+                binding.gallery.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.lightGreen));
+                binding.settings.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.lightGreen));
+                break;
 
-
+            case R.id.gallery:
+                binding.camera.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.lightGreen));
+                binding.camera.setImageTintList(ContextCompat.getColorStateList(DesginTest.this, R.color.solidGreen));
+                binding.gallery.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.selectorGreen));
+                binding.settings.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.lightGreen));
+                break;
+            case R.id.settings:
+                binding.camera.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.lightGreen));
+                binding.camera.setImageTintList(ContextCompat.getColorStateList(DesginTest.this, R.color.solidGreen));
+                binding.gallery.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.lightGreen));
+                binding.settings.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.selectorGreen));
+                break;
+        }
+    }
 }
